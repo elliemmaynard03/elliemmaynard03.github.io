e2719304f36449b190ed78ffbc24ccd137f0f48e
elliemaynard03 <EMM38@email.sc.edu>
2023-10-25 13:24:05 -0400

    uploading JSON

---
+++ projects/projects/part4/feedback.js
@@ -0,0 +1,62 @@
+const getMovies = async () => {
+    const url = "https://portiaportia.github.io/json/movies.json";
+  
+    try {
+      const response = await fetch(url);
+      return await response.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  const showMovies = async () => {
+    let movies = await getMovies();
+    console.log(movies);
+    let moviesSection = document.getElementById("movies-section");
+  
+    movies.forEach((movie) =>
+      moviesSection.append(getMovieItem(movie))
+    );
+  };
+  
+  const getMovieItem = (movie) => {
+    let movieSection = document.createElement("section");
+    movieSection.classList.add("movie");
+  
+    let a = document.createElement("a");
+    movieSection.append(a);
+  
+    let h3 = document.createElement("h3");
+    h3.innerText = movie.title;
+    a.append(h3);
+  
+    
+    let director = document.createElement("p");
+    director.textContent = `${movie.director}`;
+    a.append(director);
+
+    let actors = document.createElement("p");
+    actors.textContent = `${movie.actors}`;
+    a.append(actors);
+
+    let year = document.createElement("p");
+    year.textContent = `${movie.year}`;
+    a.append(year);
+
+    let genres = document.createElement("p");
+    genres.textContent = `${movie.genres}`;
+    a.append(genres);
+
+    let description = document.createElement("p");
+    description.textContent = `${movie.description}`;
+    a.append(description);
+
+    let image = document.createElement("img");
+    a.append(image);
+    image.src = "https://portiaportia.github.io/json/"+ movie.img;
+
+    return movieSection;
+  };
+  
+  
+  window.onload = () => showMovies();
